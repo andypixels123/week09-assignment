0d4d9b45a1d4562f4ed9efb037ddd3fa10e6f8fc
Andrew Birch <ab8218-cp@yahoo.com>
2026-01-30 16:57:03 +0000

    feat: added database connection

--- todo.js
+++ todo.js
@@ -1,6 +1,5 @@
 // todo: install pg
 // todo: create db connection
-// todo: build database tables
-// users, posts, comments
-// todo: add logic and db queries where required 
+// todo: add logic and db queries where required
+// todo: test forms
 // todo: style links the same as buttons
