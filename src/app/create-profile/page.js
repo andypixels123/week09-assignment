692d361e1e5bad0311a097b264d6739b337922bf
Andrew Birch <ab8218-cp@yahoo.com>
2026-01-30 21:50:36 +0000

    feat: create profile sends data to db and redirects user to profile page

--- src/app/create-profile/page.js
+++ src/app/create-profile/page.js
@@ -2,39 +2,55 @@ import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import { SignedIn } from "@clerk/nextjs"
 import { auth, currentUser } from "@clerk/nextjs/server"
+import { db } from "@/utils/dbConn";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
-export default async function CreateProfile() {
-
-    // get user id from Clerk
-    const { userId } = await auth();
-    // console.log(userId);
-    // get user's name from Clerk
-    const { firstName, lastName } = await currentUser();
-    // console.log(`${firstName} ${lastName}`);
+export default function CreateProfile() {
 
     // todo: store data in database
-    // function handleSubmit() {
-    // INSERT INTO social_users ();
-    // user_id, user_name, user_gender, user_age, user_hobbies
-    // }
+    async function handleSubmit(formData) {
+        "use server";
+        // get user id from Clerk
+        const { userId } = await auth();
+        // get user's name from Clerk
+        const { firstName } = await currentUser();
+        const { userGender, birthYear, userHobbies } = Object.fromEntries(formData);
+
+        //insert formValues in the database
+        db.query(
+            `INSERT INTO social_users(id, user_name, birth_year, gender, hobbies) VALUES($1, $2, $3, $4, $5)`,
+            [
+                userId,
+                firstName,
+                birthYear,
+                userGender,
+                userHobbies
+            ],
+        );
+        // revalidate
+        revalidatePath(`/profile/${userId}`);
+        // redirect
+        redirect(`/profile/${userId}`);
+    }
 
     return (
         // we are signed-in after signing-up
         <>
             <Header />
             <SignedIn>
-                <form>
-                    <label htmlFor="user-name">Name</label>
-                    <input type="text" name="user-name" defaultValue={`${firstName} ${lastName}`} required />
+                <form action={handleSubmit}>
+                    {/* <label htmlFor="user-name">Name</label>
+                <input type="text" name="user-name" defaultValue={`${firstName} ${lastName}`} required /> */}
 
-                    <label htmlFor="user-gender">Gender</label>
-                    <input type="text" name="user-gender" required />
+                    <label htmlFor="userGender">Gender</label>
+                    <input type="text" name="userGender" required />
 
-                    <label htmlFor="user-age">Year of Birth</label>
-                    <input type="number" name="user-age" required />
+                    <label htmlFor="birthYear">Year of Birth</label>
+                    <input type="number" name="birthYear" max={(new Date().getFullYear()) - 16} required />
 
-                    <label htmlFor="user-hobbies">Interests / Hobbies</label>
-                    <textarea type="text" name="user-hobbies" rows="5" required />
+                    <label htmlFor="userHobbies">Interests / Hobbies</label>
+                    <textarea type="text" name="userHobbies" rows="5" required />
 
                     <button>Submit</button>
                 </form>
--- todo.js
+++ todo.js
@@ -1,5 +1,3 @@
-// todo: install pg
-// todo: create db connection
 // todo: add logic and db queries where required
 // todo: test forms
 // todo: style links the same as buttons
